0d94c0342d6869999c61c4b9da85150eb9041d2e
jekiquin <jci.dev.0@gmail.com>
2022-04-10 00:52:10 -0700

    complete AddThought component

--- src/components/AddThought/AddThought.js
+++ src/components/AddThought/AddThought.js
@@ -1,12 +1,35 @@
+import { useState } from 'react';
+import './AddThought.scss';
+
+const MAXLENGTH = 255;
+
 export default function AddThought() {
+	const [inputText, setInputText] = useState('');
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		console.log(e.target.thought.value);
+		setInputText('');
+	};
+
+	const handleOnChange = ({ target }) => {
+		setInputText(target.value);
 	};
 	return (
-		<form onSubmit={handleSubmit}>
-			<textarea placeholder="Add a thought" id="thought"></textarea>
-			<button type="submit">Add Thought</button>
+		<form className="AddThought" onSubmit={handleSubmit}>
+			<textarea
+				id="thought"
+				className="AddThought__input"
+				placeholder="Add a thought"
+				maxLength={MAXLENGTH}
+				value={inputText}
+				onChange={handleOnChange}></textarea>
+			<p className="AddThought__counter">
+				{inputText.length}/{MAXLENGTH}
+			</p>
+			<button className="AddThought__submit" type="submit">
+				Add Thought
+			</button>
 		</form>
 	);
 }
