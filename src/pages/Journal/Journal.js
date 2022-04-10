1fe3b7c7f2cf15f875b9c59a836de0893533ea7c
jekiquin <jci.dev.0@gmail.com>
2022-04-10 00:34:09 -0700

    added partial styles

--- src/pages/Journal/Journal.js
+++ src/pages/Journal/Journal.js
@@ -1,3 +1,9 @@
+import AddThought from '../../components/AddThought/AddThought';
+
 export default function Journal() {
-	return <div>jerick</div>;
+	return (
+		<div className="Journal">
+			<AddThought />
+		</div>
+	);
 }
